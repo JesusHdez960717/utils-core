be4e4fce9b372170e87ce136b625d0efbf5c528a
Pitti <jhernandezb96@gmail.com>
2020-03-22 16:26:38 -0500

    Agregado el TypeFactory y los diferentes read a JACKSON

--- Proyecto/src/jackson/JACKSON.java
+++ Proyecto/src/jackson/JACKSON.java
@@ -7,13 +7,19 @@ package jackson;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.type.ArrayType;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  *
@@ -37,6 +43,20 @@ public class JACKSON {
         return om.readValue(src, valueType);
     }
 
+    public static <T extends Object> T read(File src, TypeReference<T> valueTypeRef) throws IOException, JsonParseException, JsonMappingException {
+        if (om == null) {
+            initObjectMapper();
+        }
+        return om.readValue(src, valueTypeRef);
+    }
+
+    public static <T extends Object> T read(File src, JavaType javaType) throws IOException, JsonParseException, JsonMappingException {
+        if (om == null) {
+            initObjectMapper();
+        }
+        return om.readValue(src, javaType);
+    }
+
     private static void initObjectMapper() {
         om = new ObjectMapper();
 
@@ -49,5 +69,13 @@ public class JACKSON {
 
     public static void registerModule(Module module) {
         om.registerModule(module);
+    }
+
+    public static TypeFactory getTypeFactory() {
+        if (om == null) {
+            initObjectMapper();
+        }
+
+        return om.getTypeFactory();
     }
 }
