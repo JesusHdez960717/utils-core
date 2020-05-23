e39b8b1b1cc9b402f02a383493b7e8893cec1d38
Unknown <unknown@example.com>
2020-05-23 12:30:30 -0500

    Agregado para que formatee meses, años y hms

--- Proyecto/src/others/SDF.java
+++ Proyecto/src/others/SDF.java
@@ -13,6 +13,9 @@ import lombok.Data;
 public class SDF {
 
     public static final SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy");
+    public static final SimpleDateFormat SDF_MONTH = new SimpleDateFormat("MMMM/yyyy");
+    public static final SimpleDateFormat SDF_YEAR = new SimpleDateFormat("yyyy");
+    public static final SimpleDateFormat SDF_HMS = new SimpleDateFormat("hh:mm:ss");
 
     public static String format(Date d) {
         return SDF.format(d);
@@ -22,4 +25,27 @@ public class SDF {
         return SDF.parse(s);
     }
 
+    public static String formatMonth(Date d) {
+        return SDF_MONTH.format(d);
+    }
+
+    public static Date parseMonth(String s) throws ParseException {
+        return SDF_MONTH.parse(s);
+    }
+
+    public static String formatYear(Date d) {
+        return SDF_YEAR.format(d);
+    }
+
+    public static Date parseYear(String s) throws ParseException {
+        return SDF_YEAR.parse(s);
+    }
+
+    public static String formatHMS(Date d) {
+        return SDF_HMS.format(d);
+    }
+
+    public static Date parseHMS(String s) throws ParseException {
+        return SDF_HMS.parse(s);
+    }
 }
