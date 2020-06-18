daf81625a8e0e2e502b260059fa25eeec63b1818
Unknown <unknown@example.com>
2020-06-18 16:34:14 -0500

    Agregado el metodo para saber la cantidad de dias entre dos fechas

--- Proyecto/src/others/Misc.java
+++ Proyecto/src/others/Misc.java
@@ -1,6 +1,8 @@
 package others;
 
+import java.util.Date;
 import java.util.Formatter;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -10,6 +12,10 @@ public class Misc {
 
     public static float round2f(double numero) {
         return Float.parseFloat(new Formatter().format("%.2f", numero).toString().replace(',', '.'));
+    }
+
+    public long daysBetween(Date from, Date to) {
+        return TimeUnit.DAYS.convert(Math.abs(to.getTime() - from.getTime()), TimeUnit.MILLISECONDS);
     }
 
     public static float evaluate(float a, String op, float b) {
