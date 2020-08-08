9b5684970cfcb38aa40f5d27b0e47b047f5241d0
Unknown <unknown@example.com>
2020-08-07 23:09:13 -0500

    JPACleanCRUDRepo: +PropertyChangeSupport. Agregado el sistema soporte de property change

--- src/main/java/com/jhw/utils/jpa/JPACleanCRUDRepo.java
+++ src/main/java/com/jhw/utils/jpa/JPACleanCRUDRepo.java
@@ -13,6 +13,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import com.clean.core.utils.SortBy;
+import com.clean.core.utils.validation.Validable;
+import com.clean.core.utils.validation.ValidationResult;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 /**
  *
@@ -21,6 +25,34 @@ import com.clean.core.utils.SortBy;
  * @param <Entity>
  */
 public class JPACleanCRUDRepo<Domain, Entity> implements CRUDRepository<Domain> {
+
+    public static final String PROP_COUNT = "count";
+    public static final String PROP_CREATE = "create";
+    public static final String PROP_DESTROY = "destroy";
+    public static final String PROP_DESTROY_BY_ID = "destroyById";
+    public static final String PROP_EDIT = "edit";
+    public static final String PROP_FIND_ALL = "findAll";
+    public static final String PROP_FIND_BY = "findBy";
+
+    private transient final java.beans.PropertyChangeSupport propertyChangeSupport = new java.beans.PropertyChangeSupport(this);
+
+    /**
+     * Add PropertyChangeListener.
+     *
+     * @param listener
+     */
+    public void addPropertyChangeListener(java.beans.PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Remove PropertyChangeListener.
+     *
+     * @param listener
+     */
+    public void removePropertyChangeListener(java.beans.PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
 
     private final JPAControllerGeneral<Entity> jpaController;
     private final Class<Domain> domainClass;
@@ -42,38 +74,68 @@ public class JPACleanCRUDRepo<Domain, Entity> implements CRUDRepository<Domain> 
 
     @Override
     public Domain create(Domain domain) throws Exception {
+        validateDomain(domain);
+
+        //copy of the object to the oldValue
+        Domain domainOld = JACKSON.convert(domain, domainClass);
+
         Entity entity = JACKSON.convert(domain, entityClass);
         jpaController.create(entity);
         domain = JACKSON.convert(entity, domainClass);
+
+        firePropertyChange(PROP_CREATE, domainOld, domain);
+
         return domain;
     }
 
     @Override
     public Domain edit(Domain domain) throws Exception {
+        validateDomain(domain);
+
+        //copy of the object to the oldValue
+        Domain domainOld = JACKSON.convert(domain, domainClass);
+
         Entity entity = JACKSON.convert(domain, entityClass);
         jpaController.edit(entity);
         domain = JACKSON.convert(entity, domainClass);
+
+        firePropertyChange(PROP_EDIT, domainOld, domain);
+
         return domain;
     }
 
     @Override
     public Domain destroy(Domain domain) throws Exception {
+        //copy of the object to the oldValue
+        Domain domainOld = JACKSON.convert(domain, domainClass);
+
         Entity entity = JACKSON.convert(domain, entityClass);
         jpaController.destroy(entity);
         domain = JACKSON.convert(entity, domainClass);
+
+        firePropertyChange(PROP_DESTROY, domainOld, domain);
+
         return domain;
     }
 
     @Override
     public Domain destroyById(Object keyId) throws Exception {
         Entity entity = jpaController.destroyById(keyId);
-        return JACKSON.convert(entity, domainClass);
+        Domain domain = JACKSON.convert(entity, domainClass);
+
+        firePropertyChange(PROP_DESTROY_BY_ID, keyId, domain);
+
+        return domain;
     }
 
     @Override
     public Domain findBy(Object keyId) throws Exception {
         Entity entity = jpaController.findBy(keyId);
-        return JACKSON.convert(entity, domainClass);
+        Domain domain = JACKSON.convert(entity, domainClass);
+
+        firePropertyChange(PROP_FIND_BY, keyId, domain);
+
+        return domain;
     }
 
     /**
@@ -102,11 +164,28 @@ public class JPACleanCRUDRepo<Domain, Entity> implements CRUDRepository<Domain> 
             Misc.sortByAnnotation(answ, domainClass, Misc.reverse(actualAnnotation.priority()), actualAnnotation.order());
         }
 
+        firePropertyChange(PROP_FIND_BY, null, answ);
+        
         return answ;
     }
 
     @Override
     public int count() throws Exception {
-        return jpaController.count();
+        int count = jpaController.count();
+        
+        firePropertyChange(PROP_COUNT, 0, count);
+        
+        return count;
+    }
+
+    private void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
+
+    private ValidationResult validateDomain(Domain domain) throws Exception {
+        if (domain instanceof Validable) {
+            return ((Validable) domain).validate().throwException();
+        }
+        throw new NullPointerException();
     }
 }
