005f4429f94df0d456dd16ff9d35119662ad229f
Unknown <unknown@example.com>
2020-09-23 22:30:12 -0500

    Repo: +Validate

--- src/main/java/com/jhw/utils/jackson/JACKSONRepoGeneral.java
+++ src/main/java/com/jhw/utils/jackson/JACKSONRepoGeneral.java
@@ -6,19 +6,21 @@
 package com.jhw.utils.jackson;
 
 import com.clean.core.app.repo.ReadWriteRepository;
+import com.clean.core.utils.validation.Validable;
+import com.clean.core.utils.validation.ValidationResult;
 import java.io.File;
 
 /**
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
- * @param <T>
+ * @param <Domain>
  */
-public abstract class JACKSONRepoGeneral<T> implements ReadWriteRepository<T> {
+public abstract class JACKSONRepoGeneral<Domain> implements ReadWriteRepository<Domain> {
 
     private transient final java.beans.PropertyChangeSupport propertyChangeSupport = new java.beans.PropertyChangeSupport(this);
 
     private final File file;
-    private final Class<T> clazz;
+    private final Class<Domain> clazz;
 
     public JACKSONRepoGeneral(String file, Class clazz) {
         this.file = new File(file);
@@ -29,13 +31,13 @@ public abstract class JACKSONRepoGeneral<T> implements ReadWriteRepository<T> {
     }
 
     @Override
-    public T read() throws Exception {
+    public Domain read() throws Exception {
         try {
-            T t = JACKSON.read(file, clazz);
+            Domain t = JACKSON.read(file, clazz);
             firePropertyChange("read", null, t);
             return t;
         } catch (Exception e) {
-            T neww = clazz.newInstance();
+            Domain neww = clazz.newInstance();
             try {
                 write(neww);
             } catch (Exception ex) {
@@ -46,7 +48,9 @@ public abstract class JACKSONRepoGeneral<T> implements ReadWriteRepository<T> {
     }
 
     @Override
-    public void write(T object) throws Exception {
+    public void write(Domain object) throws Exception {
+        validateDomain(object);
+        
         JACKSON.write(file, object);
         firePropertyChange("write", null, object);
     }
@@ -69,5 +73,12 @@ public abstract class JACKSONRepoGeneral<T> implements ReadWriteRepository<T> {
 
     private void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
         propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
+
+    private ValidationResult validateDomain(Domain domain) throws Exception {
+        if (domain instanceof Validable) {
+            return ((Validable) domain).validate().throwException();
+        }
+        return new ValidationResult();
     }
 }
