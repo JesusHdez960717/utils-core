88ef17379839c660107ffee7c1addf9fae826c89
Unknown <unknown@example.com>
2020-12-30 23:26:07 -0500

    Licence to jpa

--- src/main/java/com/root101/data/jpa/JPAControllerGeneral.java
+++ src/main/java/com/root101/data/jpa/JPAControllerGeneral.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.data.jpa;
 
-import com.root101.clean.core.app.repo.CRUDRepository;
 import com.root101.clean.core.exceptions.ValidationException;
 import com.root101.clean.core.utils.validation.Validable;
 import com.root101.clean.core.utils.validation.ValidationMessage;
@@ -13,8 +28,10 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 /**
- *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * 
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ * @param <T> 
  */
 public class JPAControllerGeneral<T> implements Validable {
 
