9870c37da0fdf08e7108b08b952a097e29829621
Unknown <unknown@example.com>
2020-12-30 23:28:41 -0500

    Rename data => repo

--- src/main/java/com/root101/repo/jpa/JPAControllerGeneralUtils.java
+++ src/main/java/com/root101/repo/jpa/JPAControllerGeneralUtils.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.root101.data.jpa;
+package com.root101.repo.jpa;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
